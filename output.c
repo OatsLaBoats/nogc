0f93e8a4f4085a0d990721d10f9d95aa56282625
Boats <48214938+OatsLaBoats@users.noreply.github.com>
2025-06-30 20:26:11 +0200

    werk werk werk werk werk...

--- output.c
+++ output.c
@@ -1,12 +1,12 @@
 #include "builtins.h"
 
+
 static const ng_Int anInt=10;
 static const struct ng_String aString=(struct ng_String){.cap=0,.len=11,.mem="Hello World"};
 
 static ng_Unit ng_main(ng_Unit);
 
 static ng_Unit ng_main(ng_Unit){
-ng_printLn(ng_cloneString(ng_sliceString(aString)));
 }
 
 
--- src/SPass1.hs
+++ src/SPass1.hs
@@ -10,20 +10,45 @@ import qualified Ir
 
 data Context = Context
     { getFunctionMap :: Map Ir.Identifier Ir.Type
+    , getFunctionAnalizedMap :: Map Ir.Identifier ()
+    , getFunctionDataMap :: Map Ir.Identifier Ir.Construct
     , getConstMap :: Map Ir.Identifier Ir.Type
+    , getIr :: [Ir.Construct]
     }
 
+isFunctionAnalyzed :: Ir.Identifier -> Context -> Bool
+isFunctionAnalyzed name ctx = Map.member name $ getFunctionAnalizedMap ctx
+
+addConstruct :: Ir.Construct -> Context -> Context
+addConstruct construct ctx = ctx { getIr = (construct : getIr ctx) }
+
 runSPass1 :: [Ir.Construct] -> [Ir.Construct]
-runSPass1 ir = undefined
+runSPass1 ir = reverse $ getIr $ foldl (flip constructPass1) context ir
     where
         context = Context
             { getFunctionMap = functionMap
+            , getFunctionAnalizedMap = functionAnalyzedMap
+            , getFunctionDataMap = functionDataMap
             , getConstMap = constMap
+            , getIr = ir
             }
+
+        functionDataMap = foldl
+            (\acc c -> case c of
+                Ir.Function name _ _ _ -> Map.insert name c acc
+                _ -> acc)
+            Map.empty ir
+
+        functionAnalyzedMap = foldl
+            (\acc c -> case c of
+                Ir.Extern name _ -> Map.insert name () acc
+                _ -> acc)
+            Map.empty ir
 
         functionMap = foldl
             (\acc c -> case c of
-                Ir.Function name _ _ _-> Map.insert name (Ir.functionConstructToType c) acc
+                Ir.Function name _ _ _ -> Map.insert name (Ir.functionConstructToType c) acc
+                Ir.Extern name type' -> Map.insert name type' acc
                 _ -> acc)
             Map.empty ir
 
@@ -32,3 +57,16 @@ runSPass1 ir = undefined
                 Ir.Constant name type' _ -> Map.insert name type' acc
                 _ -> acc)
             Map.empty ir
+
+-- In pass 1 we convert all the function parameters to be owned or borrowed
+constructPass1 :: Ir.Construct -> Context -> Context
+constructPass1 construct ctx = case construct of
+    Ir.Function _ _ _ _ -> functionPass1 construct ctx
+    _ -> addConstruct construct ctx
+
+functionPass1 :: Ir.Construct -> Context -> Context
+functionPass1 construct ctx = case construct of
+    Ir.Function name params retType expr ->
+        if isFunctionAnalyzed name ctx then ctx else
+        undefined
+    _ -> undefined
